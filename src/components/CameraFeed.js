ddb8e940b9554bb6c6cb8bd624ddbadb37973022
Unmesh Padhye <unmeshpadhye@gmail.com>
2023-04-03 18:19:23 -0700

    movement controls added

--- src/components/CameraFeed.js
+++ src/components/CameraFeed.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
-import Header from './Header';
-
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Header from "./Header";
 
 const CameraFeed = () => {
     const [stream, setStream] = useState(null);
@@ -23,10 +20,7 @@ const CameraFeed = () => {
             console.error(error);
         };
 
-        navigator.mediaDevices
-            .getUserMedia(constraints)
-            .then(success)
-            .catch(failure);
+        navigator.mediaDevices.getUserMedia(constraints).then(success).catch(failure);
 
         return () => {
             if (stream) {
@@ -35,26 +29,95 @@ const CameraFeed = () => {
         };
     }, []);
 
+    const handleDirection = (direction) => {
+        // TODO: Implement robot movement based on direction
+        console.log(`Moving robot ${direction}`);
+    };
+
+    const Object = ({ type, message, time }) => (
+        <div className="flex justify-right py-1">
+            <p className="font-bold">{type}</p>
+            <p className="ml-2 text-gray-600">{message}</p>
+            <p className="ml-2 text-gray-600">{time}</p>
+        </div>
+    );
+
+    const Objects = ({ notifications }) => (
+        <div className="bg-gray-100 rounded-md shadow-lg p-4">
+            <h2 className="font-bold mb-4">Detected Objects</h2>
+            {/*  <div>
+        {notifications.map((notification) => (
+          <Notification key={notification.type} type={notification.type} message={notification.message} time={notification.time} />
+        ))}
+      </div> */}
+        </div>
+    );
+
     return (
         <div>
             <Header title={`Camera feed`} />
-            {error ? (
-                <p>Video not available</p>
-            ) : (
-                <video
-                    style={{ maxWidth: '100%' }}
-                    autoPlay
-                    playsInline
-                    muted
-                    ref={(video) => {
-                        if (video && stream) {
-                            video.srcObject = stream;
-                        }
-                    }}
-                />
-            )}
+            <div className="flex justify-center items-center">
+                {error ? (
+                    <p>Video not available</p>
+                ) : (
+                    <>
+                        <div className="relative w-full h-full">
+                            <video
+                                width="640"
+                                height="480"
+                                style={{ maxWidth: "100%", maxHeight: "100%", objectFit: "contain" }}
+                                controls
+                                autoPlay
+                                playsInline
+                                muted
+                                ref={(video) => {
+                                    if (video && stream) {
+                                        video.srcObject = stream;
+                                    }
+                                }}
+                            />
+                            <div className="bg-slate-200 w-64 h-84 ml-60">
+                                <div className="top-10 right-0 flex flex-col justify-center items-center">
+                                    <div className="w-24 h-24 bg-white bg-opacity-90 rounded-full flex justify-center items-center ">
+                                        <button onClick={() => handleDirection("forward")} className="text-2xl">
+                                            ↑
+                                        </button>
+                                    </div>
+                                    <div className="-ml-36">
+                                        <div className="w-24 h-24 bg-white bg-opacity-90 rounded-full flex justify-center items-center">
+                                            <button onClick={() => handleDirection("left")} className="text-2xl">
+                                                ←
+                                            </button>
+                                        </div>
+                                    </div>
+
+                                    <div className="w-24 h-24 bg-white bg-opacity-90 rounded-full flex justify-center items-center mt-4">
+                                        <button onClick={() => handleDirection("back")} className="text-2xl">
+                                            ↓
+                                        </button>
+                                    </div>
+                                    <div className="-mr-36 -mt-56">
+                                        <div className="w-24 h-24 bg-white bg-opacity-90 rounded-full flex justify-center items-center mt-4">
+                                            <button onClick={() => handleDirection("right")} className="text-2xl">
+                                                →
+                                            </button>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full md:w-1/3 px-2">
+                            <div className="mb-4">
+                                <Objects />
+                            </div>
+                        </div>
+                    </>
+                )}
+            </div>
         </div>
     );
+
 };
 
 export default CameraFeed;

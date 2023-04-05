3aaa1ac57b0046a6c46dc1bc951df823563cf44c
Unmesh Padhye <unmeshpadhye@gmail.com>
2023-04-04 18:54:06 -0700

    Backend changes
    npm libraries added to package.json and DB connection file lint fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const connectDB = async ()=>{
-    try{
-    const conn = await mongoose.connect("mongodb+srv://koushik:koushik@cluster0.7jqgxjl.mongodb.net/?retryWrites=true&w=majority");
-    console.log('mongodb connected');
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect("mongodb+srv://koushik:koushik@cluster0.7jqgxjl.mongodb.net/?retryWrites=true&w=majority");
+        console.log('\n\t mongodb connected');
     }
-    catch(error){
-        console.error("error");
+    catch (error) {
+        console.error(error);
         process.exit();
 
     }

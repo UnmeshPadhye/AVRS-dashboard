44347db2f7ca57052668963a21c52fbd6f44b6c8
UnmeshPadhye <unmeshpadhye@gmail.com>
2023-05-10 17:32:06 -0700

    CORS origin issue fixed

--- backend_latest/index.js
+++ backend_latest/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 const mongoDB = require('./db/index');
+const cors = require('cors');
 // const RCL_node = require('./config/init-ros')
 const controllers = require('./routes/apiRoutes');
 
+
+// app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+    origin: "*",
+    resources: "/*"
+}));
 
 app.use('/api', ...controllers);
 

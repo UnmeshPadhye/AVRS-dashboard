88a1f6b649cf84b1e564f64ed33cf7396754d145
UnmeshPadhye <unmeshpadhye@gmail.com>
2023-05-12 15:11:58 -0700

    Robot battery chart added

--- frontend/src/components/Visualization.js
+++ frontend/src/components/Visualization.js
@@ -1,5 +1,9 @@
-import React from "react";
-import CardChart from "./ChartCard";
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+import ChartCard from "./ChartCard";
+import batteryData from "../data/chart-battery";
+import Header from './Header';
 
 const mockData = [
     { name: "Monday", value: 10 },
@@ -12,16 +16,32 @@ const mockData = [
 const Visualizations = () => {
     return (
         <div className="container-fluid">
-            <h1 className="h3 mb-2 text-gray-800">Visualizations</h1>
-            <p className="mb-4">Here are some charts to help you visualize the data:</p>
-
+            <Header title="Visualizations" />
             <div className="row">
                 <div className="col-lg-6 mb-4">
-                    <CardChart title="Errors Analysis" data={mockData} />
+                    <ResponsiveContainer width="100%" height={400}>
+                        <LineChart
+                            data={batteryData}
+                            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="time" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Line type="monotone" dataKey="level" stroke="#8884d8" activeDot={{ r: 8 }} />
+                            <Line type="monotone" dataKey="temperature" stroke="#82ca9d" />
+                            <Line type="monotone" dataKey="voltage" stroke="#ffc658" />
+                        </LineChart>
+                    </ResponsiveContainer>
                 </div>
                 <div className="col-lg-6 mb-4">
-                    <CardChart title="Performance" data={mockData} />
+                    <ChartCard title="Errors Analysis" data={mockData} />
                 </div>
+                <div className="col-lg-6 mb-4">
+                    <ChartCard title="Performance" data={mockData} />
+                </div>
+
             </div>
         </div>
     );
---
+++ frontend/src/data/chart-battery.js
@@ -0,0 +1,28 @@
+const batteryData = [
+    { time: "00:00", level: 99, temperature: 24, voltage: 12.3 },
+    { time: "01:00", level: 95, temperature: 24, voltage: 12.2 },
+    { time: "02:00", level: 92, temperature: 25, voltage: 12.1 },
+    { time: "03:00", level: 90, temperature: 25, voltage: 12.0 },
+    { time: "04:00", level: 85, temperature: 26, voltage: 11.9 },
+    { time: "05:00", level: 82, temperature: 26, voltage: 11.8 },
+    { time: "06:00", level: 79, temperature: 27, voltage: 11.7 },
+    { time: "07:00", level: 75, temperature: 27, voltage: 11.6 },
+    { time: "08:00", level: 70, temperature: 28, voltage: 11.5 },
+    { time: "09:00", level: 65, temperature: 28, voltage: 11.4 },
+    { time: "10:00", level: 62, temperature: 29, voltage: 11.3 },
+    { time: "11:00", level: 59, temperature: 29, voltage: 11.2 },
+    { time: "12:00", level: 55, temperature: 30, voltage: 11.1 },
+    { time: "13:00", level: 50, temperature: 30, voltage: 11.0 },
+    { time: "14:00", level: 48, temperature: 31, voltage: 10.9 },
+    { time: "15:00", level: 45, temperature: 31, voltage: 10.8 },
+    { time: "16:00", level: 40, temperature: 32, voltage: 10.7 },
+    { time: "17:00", level: 35, temperature: 32, voltage: 10.6 },
+    { time: "18:00", level: 32, temperature: 33, voltage: 10.5 },
+    { time: "19:00", level: 30, temperature: 33, voltage: 10.4 },
+    { time: "20:00", level: 25, temperature: 34, voltage: 10.3 },
+    { time: "21:00", level: 22, temperature: 34, voltage: 10.2 },
+    { time: "22:00", level: 19, temperature: 35, voltage: 10.1 },
+    { time: "23:00", level: 15, temperature: 35, voltage: 10.0 },
+];
+
+export default batteryData;

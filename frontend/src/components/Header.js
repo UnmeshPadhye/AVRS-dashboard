35a6ac808e907fa1607983166de21125249289de
UnmeshPadhye <unmeshpadhye@gmail.com>
2023-05-11 20:28:06 -0700

    css change

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,14 +5,13 @@ import Navbar from './Navbar';
 const Header = (Page) => {
     return (
         <nav className="text-md font-extrabold text-gray-900 dark:text-white">
-            <Navbar class=""></Navbar>
+            <Navbar class="" />
             <div className="container-fluid">
-                <div className='md:text-5xl lg:text-6xl'>
+                <div className='md:text-5xl my-3'>
                     <span className='text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400'>
                         <Link className="navbar-brand" to="/">
                             {Page.title}
                         </Link>
-
                     </span>
                 </div>
             </div>

807233f0bebe23a1d0b97df165b2b63774d0509d
UnmeshPadhye <unmeshpadhye@gmail.com>
2023-05-11 22:07:03 -0700

    Navbar added to top level App component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Navbar from './Navbar';
 
 const Header = (Page) => {
     return (
         <nav className="text-md font-extrabold text-gray-900 dark:text-white">
-            <Navbar class="" />
             <div className="container-fluid">
                 <div className='md:text-5xl my-3'>
                     <span className='text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400'>
